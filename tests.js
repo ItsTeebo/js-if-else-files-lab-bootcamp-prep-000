f6c85237813875b067fb322be9999bc3e14893b8
thibault.nicholas.a@gmail.com <thibault.nicholas.a@gmail.com>
2018-09-09 14:29:26 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,12 +1,12 @@
 var index = require("./index.js")
-
+name = "Susan"
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
-
+height = 70
 
 if (index.height === 70) {
     console.log("The height is correct")
